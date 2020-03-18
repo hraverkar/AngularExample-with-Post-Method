b1b86546a7b1745326938ee17d86bebd2ff078f5
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-03-18 14:23:41 +0530

    First commit completed application in angular using get and post method.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from "@angular/core";
+import { DataService } from "../data.service";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { HttpResponse } from "@angular/common/http";
+import { Product } from "../product";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  public products: Product[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(
+    private dataService: DataService,
+  ) {}
+
+  ngOnInit(): void {
+    this.dataService
+      .sendGetRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<Product[]>) => {
+        console.table(res);
+        this.products = res.body;
+      });
+  }
+
+  public firstPage() {
+    this.products = [];
+    this.dataService
+      .sendRequestToUrl(this.dataService.first)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+  }
+
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== "") {
+      this.products = [];
+      this.dataService
+        .sendRequestToUrl(this.dataService.next)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((res: HttpResponse<Product[]>) => {
+          console.log(res);
+          this.products = res.body;
+        });
+    }
+  }
+
+  public lastPage() {
+    this.products = [];
+    this.dataService
+      .sendRequestToUrl(this.dataService.last)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+  }
+
+  public previousPage() {
+    if (this.dataService.prev !== undefined && this.dataService.prev !== "") {
+      this.products = [];
+      this.dataService
+        .sendRequestToUrl(this.dataService.prev)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((res: HttpResponse<Product[]>) => {
+          console.log(res);
+          this.products = res.body;
+        });
+    }
+  }
+
+  buyProduct(id: any) {
+    console.log(id);
+   
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+}
